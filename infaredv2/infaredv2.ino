9e545ae20dc5f22e459f0208239c6b1c2c9e41b7
Tyler Lee <tlee753@gmail.com>
2016-09-10 21:48:27 -0400

    advanced control system

--- infaredv2/infaredv2.ino
+++ infaredv2/infaredv2.ino
@@ -2,15 +2,21 @@
  * @author Tyler Lee
  * @version 2.0
  * 
- * Arduino PIR infared/motion sensor detection program which lights onboard LED upon detection
+ * Arduino PIR infared/motion sensor detection program which lights onboard LED upon detection, serial output of analog infared distance sensor, and relay controller
  */
 
 const int LED = 13;                       // define LED ouput pin
 const int PIR = 7;                        // define infared/motion sensor input pin
+const int DIS = 0;                        // define distance sensor
+const int REL = 6;                        // define relay
+
+bool relayToggle = false;                 // initialize relay toggle
 
 void setup() {
   pinMode(LED, OUTPUT);                   // initalize LED
-  pinMode(PIR, INPUT);                    // initialize PIN 7 as infared read
+  pinMode(PIR, INPUT);                    // initialize infared/motion sensor
+  pinMode(PIR, INPUT);                    // initialize distance sensor
+  pinMode(REL, OUTPUT);                   // initialize relay
   Serial.begin(9600);                     // begin serial output
   Serial.println("Program Starting");     // test output for monitoring purposes
 }
@@ -23,6 +29,9 @@ void loop() {
     digitalWrite(LED, LOW);               // LED off
     Serial.println("No Infared and Motion Detected");
   }
-  
+  Serial.print("Distance Sensor Reading: ");
+  Serial.println(analogRead(DIS));        // print analog
+  relayToggle = !relayToggle;             // toggle the relay
+  digitalWrite(REL, relayToggle);         // write out the 
   delay(1000);                            // time delay for stability
 }
